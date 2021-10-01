70b02e47295e19980b0b201fda46d2cb2eab95be
Tapas Kumar Dash <88813256+Tapas2652@users.noreply.github.com>
2021-10-01 15:33:49 +0530

    Create StdDeviation.py

---
+++ Lab1/StdDeviation.py
@@ -0,0 +1,15 @@
+from statistics import stdev
+from fractions import Fraction as fr
+
+sample1 = (1, 2, 5, 4, 8, 9, 12)
+sample2 = (-2, -4, -3, -1, -5, -6)
+sample3 = (-9, -1, -0, 2, 1, 3, 4, 19)
+sample4 = (1.23, 1.45, 2.1, 2.2, 1.9)
+
+print("The Standard Deviation of Sample1 is % s" %(stdev(sample1)))
+							
+print("The Standard Deviation of Sample2 is % s" %(stdev(sample2)))
+							
+print("The Standard Deviation of Sample3 is % s" %(stdev(sample3)))
+								
+print("The Standard Deviation of Sample4 is % s" %(stdev(sample4)))
